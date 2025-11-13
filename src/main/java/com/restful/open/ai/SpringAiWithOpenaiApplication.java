ac3189d8287c2f40880288d91f034a4f0887b5c0
andrade <viniciusdsandrade0662@gmail.com>
2025-11-13 20:56:26 -0300

    initialize Spring Boot project with Maven and add basic configuration

---
+++ src/main/java/com/restful/open/ai/SpringAiWithOpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.restful.open.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiWithOpenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiWithOpenaiApplication.class, args);
+	}
+
+}
